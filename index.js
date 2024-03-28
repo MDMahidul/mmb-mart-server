9b7ef57f7550a6749f86f06a7656a3bc362a090c
MDMahidul <mmbmahidul007@gmail.com>
2024-03-28 19:36:02 +0600

    upload image in local folder using multer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const app=express();
+const path=require("path")
 const port =process.env.PORT || 5000;
 
 /* middlewares */
@@ -17,8 +18,30 @@ app.use(cors(corsOptions));
 
 /* db connection */
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0cmlqfw.mongodb.net/`
+  `mongodb+srv://mmbmart:mxkwL9VF9F4uWl8m@cluster0.0cmlqfw.mongodb.net/`
 );
+
+/* apis */
+/* ----------------image storage------------------ */
+const storage = multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+/* ------image upload api-------- */
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success: 1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+
 
 app.get('/',(req,res)=>{
     res.send(`MMB Mart is running on port: ${port}`);

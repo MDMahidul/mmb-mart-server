c6ecf303c735811df76a741c79c0c2511902030e
MDMahidul <mmbmahidul007@gmail.com>
2024-03-29 03:37:58 +0600

    add product data to db using mongoose schema

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken');
-const multer = require('multer');
-const app=express();
-const path=require("path")
-const port =process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const app = express();
+require("dotenv").config();
+const path = require("path");
+const port = process.env.PORT || 5000;
 
 /* middlewares */
 const corsOptions = {
@@ -18,35 +19,105 @@ app.use(cors(corsOptions));
 
 /* db connection */
 mongoose.connect(
-  `mongodb+srv://mmbmart:mxkwL9VF9F4uWl8m@cluster0.0cmlqfw.mongodb.net/`
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0cmlqfw.mongodb.net/mmb-mart`
 );
 
 /* apis */
 /* ----------------image storage------------------ */
 const storage = multer.diskStorage({
-    destination:'./upload/images',
-    filename:(req,file,cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
-})
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage });
 
 /* ------image upload api-------- */
-app.use('/images',express.static('upload/images'))
-app.post("/upload",upload.single('product'),(req,res)=>{
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+/* schema for upolading products */
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+  sub_category: {
+    type: String,
+    require: true,
+  },
+  new_price: {
+    type: Number,
+    require: true,
+  },
+  old_price: {
+    type: Number,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default:Date.now,
+  },
+  avilable: {
+    type: Boolean,
+    default:true,
+  },
+});
+
+ app.post('/addproduct',async(req,res)=>{
+    let products= await Product.find({});
+    let id;
+    if(products.length > 0){
+        let last_product_array = products.slice(-1);
+        let last_prodyct = last_product_array[0];
+        id=last_prodyct.id + 1;
+    }else{
+        id=1;
+    }
+    const product = new Product({
+      id: id,
+      name: req.body.name,
+      image: req.body.image,
+      category: req.body.category,
+      sub_category: req.body.sub_category,
+      new_price: req.body.new_price,
+      old_price: req.body.old_price,
+      name: req.body.name,
+    });
+    console.log(product);
+    await product.save();
+    console.log("saved");
     res.json({
-        success: 1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        success:true,
+        name:req.body.name,
     })
-})
+ });
 
+app.get("/", (req, res) => {
+  res.send(`MMB Mart is running on port: ${port}`);
+});
 
-
-app.get('/',(req,res)=>{
-    res.send(`MMB Mart is running on port: ${port}`);
-})
-
-app.listen(port,()=>{
-    console.log('server is running...');
-})
+app.listen(port, () => {
+  console.log("server is running...");
+});
